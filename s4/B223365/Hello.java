bbf2afcec37877c02a93a4f40957356aa5f5d39a
tut223365 <120451630+tut223365@users.noreply.github.com>
2022-12-13 17:33:01 +0900

    Update Hello.java
    merge(B223365) → Github(B223365)

--- s4/B223365/Hello.java
+++ s4/B223365/Hello.java
@@ -3,7 +3,7 @@ import java.lang.*;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("merge(B223365)");
+        System.out.println("Github(B223365)");
     }
 }
 

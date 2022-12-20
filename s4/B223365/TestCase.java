e52794ee518fee1c191041c24e01f976f975f483
”tut223365” <yasujima.yu.cl@tut.jp>
2022-12-20 17:14:04 +0900

    TestCase.java add my TestCase

--- s4/B223365/TestCase.java
+++ s4/B223365/TestCase.java
@@ -45,8 +45,10 @@ public class TestCase {
 	    freq = myObject.frequency();
 	    assert freq == 4: "Hi Ho Hi Ho, H: " + freq;
 	    // Write your testCase here
-
-
+	    myObject.setSpace("Hi Ho Hi Ho".getBytes());
+	    myObject.setTarget("Ho Hi".getBytes());
+	    freq = myObject.frequency();
+	    assert freq == 1: "Hi Ho Hi Ho, Ho Hi: " + freq;
 	}
 	catch(Exception e) {
 	    System.out.println("Exception occurred in Frequencer Object");
